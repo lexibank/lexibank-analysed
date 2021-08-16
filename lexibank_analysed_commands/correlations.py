60ac7453e36a95412a1a4cf1cba1691aee223817
Robert Forkel <xrotwang@googlemail.com>
2021-08-16 08:21:31 +0200

    get CLTS data from raw

--- lexibank_analysed_commands/correlations.py
+++ lexibank_analysed_commands/correlations.py
@@ -2,62 +2,66 @@
 Run Correlation Tests on the Results
 """
 from statistics import median
-
-from pycldf import Dataset
 from collections import defaultdict as dd
+
 from tqdm import tqdm as progressbar
 from scipy.stats import spearmanr
+from pycldf import Dataset, iter_datasets
 from pyclts import CLTS
-from cldfcatalog import Config
+from cltoolkit.models import Language, Inventory
 from cltoolkit.features import FEATURES
 from cldfzenodo import Record
 from clldutils.clilib import Table, add_format
 
-from cldfbench_lexibank_analysed import Dataset as LB
-from cltoolkit.models import Language, Inventory
+from cldfbench_lexibank_analysed import Dataset as LB, CLTS_2_1
 
 
 def register(parser):
     add_format(parser)
 
 
+def get_cldf_dataset(doi, directory):
+    if directory.exists():
+        return next(iter_datasets(directory))
+    return Dataset.from_metadata(Record.from_doi(doi).download_dataset(directory))
+
+
 def run(args):
     lba = LB()
-    
-    clts = CLTS(Config.from_file().get_clone("clts"))
+
+    args.log.info('Loading data ...')
+    clts = CLTS(lba.raw_dir / CLTS_2_1[1])
     clts2phoible = clts.transcriptiondata_dict["phoible"]
 
     # WALS Online v2020.1
-    wmd = Record.from_doi('10.5281/zenodo.4683137').download_dataset(lba.raw_dir / 'wals')
+    wals = get_cldf_dataset('10.5281/zenodo.4683137', lba.raw_dir / 'wals')
     # PHOIBLE 2.0.1
-    pmd = Record.from_doi('10.5281/zenodo.2677911').download_dataset(lba.raw_dir / 'phoible')
+    phoible = get_cldf_dataset('10.5281/zenodo.2677911', lba.raw_dir / 'phoible')
 
-    phoible = Dataset.from_metadata(pmd)
-    wals = Dataset.from_metadata(wmd)
     lexicore = Dataset.from_metadata(lba.cldf_dir / "phonology-metadata.json")
 
     by_gcode = dd(lambda: dd(lambda: dd(lambda: dd(dict))))
-    
-    # transform wals data
+
+    args.log.info('... LexiCore ...')
     lexicorekeys = {lg.id: lg for lg in lexicore.objects("LanguageTable")}
 
-    for value in progressbar(lexicore.objects("ValueTable"), desc="loading lexicore data2"):
+    for value in lexicore.objects("ValueTable"):
         if lexicorekeys[value.cldf.languageReference].cldf.glottocode:
             lg = lexicorekeys[value.cldf.languageReference]
             by_gcode[lg.cldf.glottocode]["lexicore"][lg.id][value.cldf.parameterReference] = \
                 value.cldf.value
 
-    # transform phoible data
+    args.log.info('... PHOIBLE ...')
     phoiblekeys = {lg.id: lg for lg in phoible.objects("LanguageTable")}
     phoibledata = dd(lambda: dd(list))
 
-    for value in progressbar(phoible.objects("ValueTable"), desc="phoible data2"):
+    for value in phoible.objects("ValueTable"):
         language = value.cldf.languageReference
         contribution = value.data["Contribution_ID"]
         phoibledata[language][contribution].extend([
             clts2phoible.grapheme_map.get(value.cldf.value, "?")])
 
-    for gcode in progressbar(phoibledata):
+    for gcode in progressbar(phoibledata, desc='extracting PHOIBLE inventories'):
         for lid, sounds in phoibledata[gcode].items():
             if not "?" in sounds:        
                 language = Language(
@@ -68,13 +72,14 @@ def run(args):
                 language.sound_inventory = Inventory.from_list(clts.bipa, *sounds)
                 by_gcode[gcode]["phoible"][lid] = language
 
-    # transform wals data
+    args.log.info('... WALS ...')
     walskeys = {lg.id: lg for lg in wals.objects("LanguageTable")}
-    for value in progressbar(wals.objects("ValueTable"), desc="loading wals data2"):
+    for value in wals.objects("ValueTable"):
         if walskeys[value.cldf.languageReference].cldf.glottocode:
             lg = walskeys[value.cldf.languageReference]
             by_gcode[lg.cldf.glottocode]["wals"][lg.id][value.cldf.parameterReference] = \
                 value.cldf.value
+    args.log.info('... done')
 
     features = [
         ("1A", "ConsonantSize", FEATURES["ConsonantSize"], int),
@@ -84,28 +89,21 @@ def run(args):
         ("5A", "PlosiveVoicingGaps", FEATURES["PlosiveVoicingGaps"], int),
     ]
 
+    args.log.info('Computing correlations ...')
     comparisons = {row[0]: [] for row in features}
     with Table(args, "Feature", "WALS/LexiCore", "WALS/PHOIBLE", "LexiCore/PHOIBLE", "N") as table:
         for gcode, dsets in by_gcode.items():
             if len(dsets) == 3:
                 for d1, d2, d3, convert in features:
-                    # start with wals
-                    wals_values = []
-                    for dc, values in dsets["wals"].items():
-                        if values.get(d1):
-                            wals_values += [convert(values[d1])]
-                    lexicore_values = []
-                    for dc, values in dsets["lexicore"].items():
-                        if values.get(d2):
-                            lexicore_values += [convert(values[d2])]
-                    phoible_values = []
-                    for dc, values in dsets["phoible"].items():
-                        phoible_values += [d3(values)]
+                    wals_values = [
+                        convert(vals[d1]) for vals in dsets["wals"].values() if vals.get(d1)]
+                    lexicore_values = [
+                        convert(vals[d2]) for vals in dsets["lexicore"].values() if vals.get(d2)]
+                    phoible_values = [d3(vals) for vals in dsets["phoible"].values()]
+
                     if wals_values and lexicore_values and phoible_values:
-                        comparisons[d1] += [[
-                            median(wals_values),
-                            median(lexicore_values),
-                            median(phoible_values)]]
+                        comparisons[d1].append([
+                            median(wals_values), median(lexicore_values), median(phoible_values)])
         for d1, d2, d3, _ in features:
             p1, r1 = spearmanr([x[0] for x in comparisons[d1]], [x[1] for x in comparisons[d1]])
             p2, r2 = spearmanr([x[0] for x in comparisons[d1]], [x[2] for x in comparisons[d1]])
