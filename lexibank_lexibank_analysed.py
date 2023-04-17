c3ba31a57a70bbb8e7abea636243966070348720
LinguList <mattis.list@lingpy.org>
2023-04-17 12:05:28 +0200

    update fix final problem

--- lexibank_lexibank_analysed.py
+++ lexibank_lexibank_analysed.py
@@ -83,6 +83,7 @@ class CustomLanguage(Language):
     FormsWithSounds = attr.ib(default=None)
     Concepts = attr.ib(default=None)
     Incollections = attr.ib(default=None)
+    Dataset = attr.ib(default=None)
 
 
 class Dataset(BaseDataset):
