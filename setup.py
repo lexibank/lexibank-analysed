ffb0f56421042660e25bbbcf646c46e4dd7fa835
Johannes Englisch <johannes_englisch@eva.mpg.de>
2025-01-10 12:32:54 +0100

    update supported python versions

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         ],
     },
     platforms='any',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     install_requires=[
         'collabutils[googlesheets]',
         'cldfbench>=1.7.2',
@@ -63,10 +63,12 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
