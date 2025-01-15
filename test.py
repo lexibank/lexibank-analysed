055a7bce7ab20a818656b8efa0357b888690494a
Johannes Englisch <johannes_englisch@eva.mpg.de>
2025-01-15 09:30:30 +0100

    separate tests for the different data sets

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
-from pycldf import iter_datasets
+from pycldf import iter_datasets, Dataset
 
 
-def test_valid(cldf_dataset, cldf_logger):
-    for ds in iter_datasets(cldf_dataset.directory):
-        assert ds.validate(log=cldf_logger)
+def test_lexicon(cldf_dataset, cldf_logger):
+    md = cldf_dataset.directory / 'lexicon-metadata.json'
+    ds = Dataset.from_metadata(md)
+    assert ds.validate(log=cldf_logger)
+
+
+def test_phonemes(cldf_dataset, cldf_logger):
+    md = cldf_dataset.directory / 'phonemes-metadata.json'
+    ds = Dataset.from_metadata(md)
+    assert ds.validate(log=cldf_logger)
+
+
+def test_phonology(cldf_dataset, cldf_logger):
+    md = cldf_dataset.directory / 'phonology-metadata.json'
+    ds = Dataset.from_metadata(md)
+    assert ds.validate(log=cldf_logger)
+
+
+def test_wordlist(cldf_dataset, cldf_logger):
+    md = cldf_dataset.directory / 'wordlist-metadata.json'
+    ds = Dataset.from_metadata(md)
+    assert ds.validate(log=cldf_logger)
