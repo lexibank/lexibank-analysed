fd0b1d6b8db538805a173dc3f1dadbf520b3bab8
Mattis List <mattis.list@uni-passau.de>
2025-03-27 10:34:13 +0100

    finalize for v2.1

--- lexibank_lexibank_analysed.py
+++ lexibank_lexibank_analysed.py
@@ -353,14 +353,18 @@ class Dataset(BaseDataset):
             if not langs:
                 try:
                     family = languoids[language.glottocode].family
-                    macroarea = languoids[language.glottocode].macroareas
+                    macroareas = languoids[language.glottocode].macroareas
+                    if not macroareas:
+                        macroareas = [l.macroareas[0].name for l in
+                                      languoids[language.glottocode].iter_descendants()
+                                      if l.macroareas]
 
                     langs = {
                         "ID": language.id,
                         "Name": language.name,
                         "Glottocode": language.glottocode,
                         "Dataset": language.dataset,
-                        "Macroarea": macroarea[0].name if macroarea else "",
+                        "Macroarea": macroareas[0],
                         "Latitude": language.latitude,
                         "Longitude": language.longitude,
                         "Subgroup": language.subgroup,
